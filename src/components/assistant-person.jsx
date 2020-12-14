132c5377b1edfbef51056b7f57ce47325970e3ba
walidmahade <walid_mahade@outlook.com>
2020-12-14 11:10:48 +0600

    basic home screen created

--- src/components/assistant-person.jsx
+++ src/components/assistant-person.jsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import VideoPosterImg from '../images/video-poster.png'
+import '../styles/components/_video-assistant.scss'
 
 export default function AssistantPerson() {
   return (
-    <div className='videos-wrap'>
-      <img src={VideoPosterImg} alt='video poseter' />
+    <div className='assistant-person'>
+      <div className='container'>
+        <div className='videos-wrap'>
+          <img src={VideoPosterImg} alt='video poseter' />
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/optionsAndInfoBox.js
+++ src/components/optionsAndInfoBox.js
@@ -3,15 +3,21 @@ import '../styles/components/_options-and-text-box.scss'
 
 export default function OptionsAndInfoBox() {
   return (
-    <div className='text-box'>
-      <p>Torstein collects information necessary to send you its best offer.</p>
+    <div className='options-box'>
+      <div className='container'>
+        <div className='text-box'>
+          <p>
+            Torstein collects information necessary to send you its best offer.
+          </p>
 
-      <p>
-        This information is not shared with third parties and should only be
-        used in dialogue between VKSYS and you as a customer.
-      </p>
+          <p>
+            This information is not shared with third parties and should only be
+            used in dialogue between VKSYS and you as a customer.
+          </p>
 
-      <p>"GDPR" text must be entered here</p>
+          <p>"GDPR" text must be entered here</p>
+        </div>
+      </div>
     </div>
   )
 }
