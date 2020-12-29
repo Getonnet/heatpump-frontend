af296fa32d95090889e7e53468b6e182c5052790
walidmahade <walid_mahade@outlook.com>
2020-12-29 20:41:31 +0600

    small changes

--- src/components/assistant-person.jsx
+++ src/components/assistant-person.jsx
@@ -1,24 +1,41 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 // import VideoPosterImg from '../images/video-poster.png'
 import '../styles/components/_video-assistant.scss'
 
 export default function AssistantPerson() {
-  const firstVideo = useRef()
-  // console.log(firstVideo)
+  const firstVideo = useRef(null)
 
-  // useEffect(() => {
-  // firstVideo.current.play()
-  // }, [])
+  const [activeVideo, setActiveVideo] = useState('greetings')
+
+  const handleVideoEnd = () => {
+    setActiveVideo('idle')
+  }
 
   return (
     <div className='assistant-person'>
       <div className='container'>
         <div className='videos-wrap'>
           {/*<img src={VideoPosterImg} alt='video poseter' />*/}
-          <video controls={false} autoPlay muted ref={firstVideo}>
-            <source src='/videos/01_HELLO_480.mp4' type={'video/mp4'} />
-            Your browser does not support the video tag.
-          </video>
+          {activeVideo === 'greetings' ? (
+            <video
+              controls={false}
+              autoPlay
+              muted
+              ref={firstVideo}
+              onEnded={handleVideoEnd}>
+              {/*-- say hello --*/}
+              <source src='/videos/01_HELLO_480.mp4' type={'video/mp4'} />
+              Your browser does not support the video tag.
+            </video>
+          ) : activeVideo === 'idle' ? (
+            <video controls={false} autoPlay muted loop ref={firstVideo}>
+              {/*-- stare loop --*/}
+              <source src='/videos/00_MAIN_480.mp4' type={'video/mp4'} />
+              Your browser does not support the video tag.
+            </video>
+          ) : (
+            ''
+          )}
         </div>
       </div>
     </div>
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const configure = {
-  API_URL: 'https://hit.getonnet.dev/',
+  API_URL: 'http://localhost:3000/api/',
+  API_URL_LIVE: 'https://hit.getonnet.dev/',
 }
 
 export default configure
