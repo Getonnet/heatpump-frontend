6dcea78fbb723e3fee40e2d902b2bbb00aa0a8c7
hassantvs24 <hassantvs24@gmail.com>
2021-01-15 13:44:25 +0600

    Update UI

--- src/components/gdprNotice.jsx
+++ src/components/gdprNotice.jsx
@@ -18,20 +18,25 @@ export default function GDPRNotice(props) {
 
   return (
     <OptionsAndInfoBox>
+      
       <p> {t('collects')}</p>
       <p> {t('information')}</p>
       <p> {t('GDPR')}</p>
+      
       {/* <p>XXXXX collects information necessary to send you its best offer.</p> */}
       {/* <p>
         This information is not shared with third parties and should only be
         used in dialogue between VKSYS and you as a customer.
       </p> */}
       {/* <p>"GDPR" text must be entered here</p> */}
-      <div className='gdpr-cta' style={{ marginTop: '20px' }}>
-        <button className='btn' onClick={handleClick}>
+      <div className='gdpr-cta' style={{ marginTop: '20px', marginBottom:'20px', position: 'relative'}}>
+        <button className='btn' style={{position: 'absolute', zIndex: 10010 }} onClick={handleClick}>
           {t('accept')}
         </button>
+        <br />
+        <div style={{ position: 'fixed', left: 0, right: 0, top: 0, bottom: 0, zIndex: 10000 }} ></div>
       </div>
+
     </OptionsAndInfoBox>
   )
 }
